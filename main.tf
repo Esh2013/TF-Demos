ecd12b21236ba21d5955d60fd773e52ba186338e
Esh2013 <166420049+Esh2013@users.noreply.github.com>
2024-04-08 22:26:24 +0530

    update

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+resource "aws_vpc" "test_vpc" {
+cidr_block="10.0.0.0/16"
+instance_tenancy="default"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "VPC_test"
   }
 }
